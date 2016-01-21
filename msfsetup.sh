96c7748a3cf86d3d94544ebb02b311482681858c
sealmindset <sealmindset@gmail.com>
2016-01-21 11:04:23 -0600

    Update msfsetup.sh

--- msfsetup.sh
+++ msfsetup.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 #
 # Metasploit initial setup after fresh install
-service postgres status
-if [ "$?" -gt "0" ]; then
-  service postgresql start
-  update-rc.d postgresql defaults
-  msfdb init
+mdbname="msfdb"
+srvname="postgre"
+# Check if certain packages are installed
+arrayPKG=( postgresql )
+for pkgname in "${arrayPKG[@]}"
+do
+    if [ $(dpkg-query -W -f='${Status}' $pkgname 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+        echo "apt-get install $pkgname"
+    else
+        echo "$pkgname is installed"
+    fi
+done
+# Check if postgresql is started
+if [ ps auxw | grep $srvname | grep -v grep > /dev/null ]; then
+    echo "Postgresql has not been started"
+    service postgresql start
+    update-rc.d postgresql defaults
+else
+    echo "Postgresql has been already started"
+fi
+# Check if the msfdb install installed
+if [ psql -lqt | cut -d \| -f 1 | grep -w $mdbname ]; then
+    echo "$mdb has already been installed"
+else
+    echo "Installing $mdb now"
+    msfdb init
 fi
