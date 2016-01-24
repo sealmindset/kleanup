2092c21a3d09e22c54f13c1691cded9c71440b71
sealmindset <sealmindset@gmail.com>
2016-01-24 07:09:25 -0600

    Update kleanup.sh

--- kleanup.sh
+++ kleanup.sh
@@ -4,24 +4,24 @@ apt-get autoremove
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
-if [ $(type deborphan | wc -l) < 1 ]; then
-  echo 'deborphan is not installed.' >&2
+if [ $(type deborphan | wc -l) -lt 1 ]; then
+  echo 'deborphan is not installed.'
   apt-get install deborphan
 else
   apt-get remove --purge $(deborphan)
 fi
-if [ $(type git | wc -l) < 1 ]; then
-  echo 'git is not installed.' >&2
+if [ $(type git | wc -l) -lt 1 ]; then
+  echo 'git is not installed.'
   apt-get install git
 fi
-if [ $(type localepurge | wc -l) < 1 ]; then
-  echo 'localpurge is not installed.' >&2
+if [ $(type localepurge | wc -l) -lt 1 ]; then
+  echo 'localpurge is not installed.'
   apt-get install localepurge
 else
   localepurge
 fi
-if [ $(type ncdu | wc -l) < 1 ]; then
-  echo 'ncdu is not installed.' >&2
+if [ $(type ncdu | wc -l) -lt 1 ]; then
+  echo 'ncdu is not installed.'
   apt-get install ncdu
 else
   ncdu
