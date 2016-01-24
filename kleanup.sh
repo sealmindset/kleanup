bb2ede08bd755ec70c4c5c7b8dff9f4850e00193
sealmindset <sealmindset@gmail.com>
2016-01-24 06:55:35 -0600

    Update kleanup.sh

--- kleanup.sh
+++ kleanup.sh
@@ -4,23 +4,23 @@ apt-get autoremove
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
-if ! [ -x "$(command -v deborphan)" ]; then
+if [ $(type deborphan | wc -l) < 1 ]; then
   echo 'deborphan is not installed.' >&2
   apt-get install deborphan
 else
   apt-get remove --purge $(deborphan)
 fi
-if ! [ -x "$(command -v git)" ]; then
+if [ $(type git | wc -l) < 1 ]; then
   echo 'ncdu is not installed.' >&2
   apt-get install git
 fi
-if ! [ -x "$(command -v localepurge)" ]; then
+if [ $(type localepurge | wc -l) < 1 ]; then
   echo 'localpurge is not installed.' >&2
   apt-get install localepurge
 else
   localepurge
 fi
-if ! [ -x "$(command -v ncdu)" ]; then
+if [ $(type ncdu | wc -l) < 1 ]; then
   echo 'ncdu is not installed.' >&2
   apt-get install ncdu
 else
