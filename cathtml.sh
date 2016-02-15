ed56bd6ddfa4b5f6cfa70c990c47380301acc7a5
sealmindset <sealmindset@gmail.com>
2016-02-15 12:37:50 -0600

    Update cathtml.sh

--- cathtml.sh
+++ cathtml.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-cat index.html | grep "href" | cut -d"/" -f3| grep "cisco\.com" | cut -d'"' -f1 | sort -u
+#cat index.html | grep "href" | cut -d"/" -f3| grep "cisco\.com" | cut -d'"' -f1 | sort -u > cisco.txt
+# or
+grep -o '[A-Za-z0-9_\.-]*\.*cisco.com' index.html | sort -u > cisco.txt
