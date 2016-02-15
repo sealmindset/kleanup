dfd58e7e915b928053fac26875f423bcc043daed
sealmindset <sealmindset@gmail.com>
2016-02-15 10:59:04 -0600

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -3,7 +3,10 @@
 echo "What server to dyou want to connect to?"
 read $rdpsrvr
 
+echo "Who should I login as"
+read $user
+
 echo "What password should I use?"
 read $password
 
-rdesktop -u offsec -p $password $rdpsrvr
+rdesktop -u $user -p $password $rdpsrvr
