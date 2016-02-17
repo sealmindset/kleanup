108fe24e5b714f78978084a8b1f74ca8b5ecf8f4
sealmindset <sealmindset@gmail.com>
2016-02-17 07:36:29 -0600

    Update kleanup.sh

--- kleanup.sh
+++ kleanup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-distro=$(lsb_release -si | awk '{ print $1 }')
+distro=$(lsb_release -si)
+version=$(lsb_release -d | cut -d" " -f3)
+header=$(uname -r | cut -d"-" -f1)
 apt-get autoclean
 apt-get clean
 apt-get autoremove
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
+if [ $version == "Rolling" ]; then
+  if [ $header == "4.3.0"]; then
+    echo 'Header $header is already installed.'
+  else
+    apt-get install linux-headers-4.3.0-kali-all
+  fi
+fi
 if [ $(type deborphan | wc -l) -lt 1 ]; then
   echo 'deborphan is not installed.'
   apt-get install deborphan
