77fe216dfb7a45ec19eac03962b6f53e560a7396
sealmindset <sealmindset@gmail.com>
2018-04-23 19:30:22 -0500

    update

--- ksetup.sh
+++ ksetup.sh
@@ -4,25 +4,15 @@ YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-if [ ! -f /etc/apt/apt.conf.d/90forceyes ]; then
-  echo "APT::Get::Assume-Yes "true";" >> /etc/apt/apt.conf.d/90forceyes
-  echo "APT::Get::force-yes "true"; "true";" >> /etc/apt/apt.conf.d/90forceyes
-fi
-
 # Dist Upgrade once
 if [ $(grep dist-upgrade /var/log/apt/history.log | wc -l) -lt 1 ]; then
   echo -e "${RED} [!!!] ${NC} Performing a Distribution Upgrade!"
-  apt-get --yes --force-yes update && apt-get --yes --force-yes upgrade && apt-get --yes --force-yes dist-upgrade
+  apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade
 else
   echo -e "${GREEN} [!] ${NC} A Full Dist was already completed."
-  if [ ! -f /var/log/apt/term.log ]; then
+  if [ $(($(date +%s) - $(date +%s -r /var/log/apt/term.log))) -gt 3600 ]; then
     echo -e "${YELLOW} [!] ${NC} Do a apt-get update"
-    apt-get --yes --force-yes update
-  else
-    if [ $(($(date +%s) - $(date +%s -r /var/log/apt/term.log))) -gt 3600 ]; then
-      echo -e "${YELLOW} [!] ${NC} Do a apt-get update"
-      apt-get --yes --force-yes update
-    fi
+    apt-get update
   fi
 fi
 
@@ -43,12 +33,16 @@ done
 }
 
 # Install bare
-aptArray=(hexchat exploitdb exploitdb-papers exploitdb-bin-sploits)
+aptArray=(hexchat libssl1.0-dev exploitdb exploitdb-papers exploitdb-bin-sploits)
 installAPT "${aptArray[@]}"
 
 # Install PhantomJS & Imagemagick
 imgArray=(build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 imagemagick)
 installAPT "${imgArray[@]}"
+
+trmArray=(terminator)
+installAPT "${trmArray[@]}"
+
 
 if [ $(type phantomjs | wc -l) -lt 1 ]; then
   echo -e "${YELLOW} [!] ${NC} Installing phantomjs!"
@@ -59,30 +53,54 @@ else
   echo -e "${GREEN} [*] ${NC} phantomjs is already installed"    
 fi
 
-if [ ! -d /root/projects ]; then
+if [ -d /root/projects ]; then
+  echo -e "${GREEN} [*] ${NC} Directory for projects already exists!"    
+else  
   echo -e "${YELLOW} [!] ${NC} Creating the directory for projects."
   mkdir /root/projects
-else
-  echo -e "${GREEN} [*] ${NC} Directory for projects already exists!"    
 fi
 
-if [ ! -d /root/wordlist ]; then
+if [ -d /root/wordlist ]; then
+  echo -e "${GREEN} [*] ${NC} wordlist directory already exists!"    
+  if [ -d /root/wordlist/RobotsDisallowed ]; then
+    echo -e "${GREEN} [*] ${NC} RobotsDisallowed directory already exists!"    
+  else
+    echo -e "${YELLOW} [!] ${NC} Cloneing RobotsDisallowed ..."
+    cd /root/wordlist
+    git clone https://github.com/danielmiessler/RobotsDisallowed.git
+  fi
+  if [ -d /root/wordlist/SecLists ]; then
+    echo -e "${GREEN} [*] ${NC} SecLists directory already exists!"    
+  else
+    echo -e "${YELLOW} [!] ${NC} Cloneing SecLists ..."
+    cd /root/wordlist
+    git clone https://github.com/danielmiessler/SecLists.git
+  fi
+else
   echo -e "${YELLOW} [!] ${NC} Creating wordlist..."
   mkdir /root/wordlist
+  cd /root/wordlist
+  echo -e "${YELLOW} [!] ${NC} Cloneing RobotsDisallowed ..."
   git clone https://github.com/danielmiessler/RobotsDisallowed.git
-  mv -R ${PWD}/RobotsDisallowed /root/wordlist/
+  echo -e "${YELLOW} [!] ${NC} Cloneing SecLists ..."
   git clone https://github.com/danielmiessler/SecLists.git
-  mv -R ${PWD}/SecLists /root/wordlist/
-else
-  echo -e "${GREEN} [*] ${NC} wordlist directory already exists!"    
 fi
 
-if [ ! -d /root/oscp ]; then
-  echo -e "${YELLOW} [!] ${NC} Grabbing the OSCP toolbox!"
-  git clone https://github.com/sealmindset/oscp
-  mv -R ${PWD}/oscp /root/
-else
+if [ -d /root/oscp ]; then
   echo -e "${GREEN} [*] ${NC} OSCP directory already exists!"  
+else
+  echo -e "${YELLOW} [!] ${NC} Grabbing the OSCP toolbox!"
+  cd /root
+  git clone https://github.com/sealmindset/oscp
 fi
+
+apt-get autoclean
+apt-get autoremove
 
 echo -e "${GREEN} [*] ${NC} Now go hack something... (legally)"
+cd /root/oscp
+
+echo -e "${GREEN} [*] ${NC} Setting up Autologin... (legally)"
+sed -i '/^#.* AutomaticLogin/s/^#//' /etc/gdm3/daemon.conf
+
+reboot
