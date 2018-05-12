a86abf8e2efbe1513a0b74ba0f0677d247747043
sealmindset <sealmindset@gmail.com>
2018-05-12 12:05:08 -0500

    Adding more tools

--- ksetup.sh
+++ ksetup.sh
@@ -44,11 +44,11 @@ function gitClone() {
   arr=("$@")
   for k in "${arr[@]}"; do
     dirSTR=$(echo ${k} | sed 's/.*\///' | sed 's/\.git//')
-    if [ -d /root/recon/${dirSTR} ]; then
+    if [ -d /opt/${dirSTR} ]; then
       echo -e "${GREEN} [!] ${NC} Got ${dirSTR}"
     else
       echo -e "${YELLOW} [!] ${NC} Git'n ${k}"
-      git clone https://github.com/${k} /root/recon/${dirSTR}
+      git clone https://github.com/${k} /opt/${dirSTR}
     fi
     dirSTR=""
   done
@@ -57,12 +57,17 @@ function gitClone() {
 ####> Not quite PTF, but its the tools I'm using 
 
 aptArray=(hexchat terminator exploitdb exploitdb-papers exploitdb-bin-sploits golang-go libesedb-utils gcc-mingw-w64-i686)
+
+installAPT "${aptArray[@]}"
+
+aptArray=(veil)
+
 installAPT "${aptArray[@]}"
 
 if [ $(type phantomjs | wc -l) -lt 1 ]; then
   echo -e "${YELLOW} [!] ${NC} Installing phantomjs!"
   imgArray=(build-essential chrpath libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 imagemagick)
-installAPT "${imgArray[@]}"
+  installAPT "${imgArray[@]}"
   wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
   tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
   ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
@@ -83,52 +88,53 @@ if [ -d /root/wordlist ]; then
     echo -e "${GREEN} [*] ${NC} RobotsDisallowed directory already exists!"    
   else
     echo -e "${YELLOW} [!] ${NC} Cloneing RobotsDisallowed ..."
-    cd /root/wordlist
-    git clone https://github.com/danielmiessler/RobotsDisallowed.git
+    git clone https://github.com/danielmiessler/RobotsDisallowed.git /root/wordlist/RobotsDisallowed
   fi
   if [ -d /root/wordlist/SecLists ]; then
     echo -e "${GREEN} [*] ${NC} SecLists directory already exists!"    
   else
     echo -e "${YELLOW} [!] ${NC} Cloneing SecLists ..."
-    cd /root/wordlist
-    git clone https://github.com/danielmiessler/SecLists.git
+    git clone https://github.com/danielmiessler/SecLists.git /root/wordlist/Seclists
   fi
 else
   echo -e "${YELLOW} [!] ${NC} Creating wordlist..."
   mkdir /root/wordlist
-  cd /root/wordlist
   echo -e "${YELLOW} [!] ${NC} Cloneing RobotsDisallowed ..."
-  git clone https://github.com/danielmiessler/RobotsDisallowed.git
+  git clone https://github.com/danielmiessler/RobotsDisallowed.git /root/wordlist/RobotsDisallowed
   echo -e "${YELLOW} [!] ${NC} Cloneing SecLists ..."
-  git clone https://github.com/danielmiessler/SecLists.git
+  git clone https://github.com/danielmiessler/SecLists.git /root/wordlist/SecLists
 fi
 
 if [ -f /usr/share/nmap/scripts/banner-plus.nse ]; then
   echo -e "${GREEN} [*] ${NC} banner-plus.nse exist! " 
 else
   echo -e "${YELLOW} [*] ${NC} Grabbing banner-plus.nse... " N
-  cd /root/wappass
-  git clone https://github.com/hdm/scan-tools.git
-  cp /root/wappass/scan-tools/nse/banner-plus.nse /usr/share/nmap/scripts/
+  git clone https://github.com/hdm/scan-tools.git /tmp/scan-tools
+  cp /tmp/scan-tools/nse/banner-plus.nse /usr/share/nmap/scripts/
 fi
 
-if [ -d /root/wappass/w3af ]; then
+if [ -d /opt/w3af ]; then
   echo -e "${GREEN} [*] ${NC} w3af is installed! " 
 else
   echo -e "${YELLOW} [*] ${NC} Grabbing w3af... " N
-  cd /root/wappass
-  git clone https://github.com/andresriancho/w3af.git
+  git clone https://github.com/andresriancho/w3af.git /opt/w3af
 
   w3afArray=(build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev)
   installAPT "${w3afArray[@]}"
   
   w3af2Array=(libxml2-dev libxslt1-dev zlib1g-dev)
+
   installAPT "${w3af2Array[@]}"
+
   pip install lxml==3.4.4 scapy-real==2.2.0-dev guess-language==0.2 cluster==1.1.1b3 msgpack-python==0.4.4 python-ntlm==1.0.1 halberd==0.2.4 darts.util.lru==0.5 vulndb==0.0.19 markdown==2.6.1 psutil==2.2.1 ds-store==1.1.2 termcolor==1.1.0 mitmproxy==0.13 ruamel.ordereddict==0.4.8 Flask==0.10.1 tldextract==1.7.2 pebble==4.3.6 acora==2.1 esmre==0.3.1
 
-  cd /root/wappass/w3af/
-  ./w3af_console
-  ./tmp/w3af_dependency_install.sh
+  pip install pyOpenSSL==17.4.0 bravado==9.2.2
+  pip install xdot==0.6
+  pip install lxml==3.4.4
+  apt-get install graphviz
+
+  cd /tmp/w3af
+  ./w3af_dependency_install.sh
 fi
 
 # Not sure what the intersecs or the diffs are... yet!
@@ -146,7 +152,6 @@ if [ -f /usr/bin/pwsh ]; then
   echo -e "${GREEN} [*] ${NC} PowerShell is installed! " 
 else
   echo -e "${YELLOW} [*] ${NC} Grabbing PowerShell... " N
-  cd /root
 
   psArray=(libunwind8 liblttng-ust0)
   installAPT "${psArray[@]}"
@@ -162,31 +167,30 @@ else
   fi
 fi
 
-if [ -d /root/redteam/powershell/Empire ]; then
+if [ -d /opt/Empire ]; then
   echo -e "${GREEN} [*] ${NC} Empire Strikes Back!"  
 else
   echo -e "${YELLOW} [!] ${NC} Git Empire...!"  
   pip install lxml==4.0.0
-  cd /root/redteam/powershell
-  git clone https://github.com/EmpireProject/Empire.git
-  cd /root/redteam/powershell/Empire/setup
+  git clone https://github.com/EmpireProject/Empire.git /opt/Empire
+  cd /opt/Empire/setup
   ./install.sh
   ./reset.sh
 fi
 
-if [ -d /root/redteam/dnscat2 ]; then
+if [ -d /opt/dnscat2 ]; then
   echo -e "${GREEN} [*] ${NC} Meow!"  
 else
   echo -e "${YELLOW} [!] ${NC} Git some pussy...!"  
   dnsArray=(ruby-dev gcc)
   installAPT "${dnsArray[@]}"
   cd /root
-  git clone https://github.com/iagox86/dnscat2.git
-  cd /root/dnscat2/server/  
+  git clone https://github.com/iagox86/dnscat2.git /opt/dnscat2
+  cd /opt/dnscat2/server/  
   make  
   gem install bundler
   bundle install  
-  cd /root/dnscat2/client/  
+  cd /opt/dnscat2/client/  
   make
   echo -e "${GREEN} [!] ${NC} Test it: ruby ./dnscat2.rb"
 fi
@@ -204,12 +208,12 @@ else
   echo -e "${YELLOW} [!] ${NC} Run: code --user-data-dir=\"~/.vscode-root\""
 fi
 
-if [ -f /root/redteam/shell/pupy/pupy/pupysh.py ]; then
+if [ -f /opt/pupy/pupy/pupysh.py ]; then
   echo -e "${GREEN} [!] ${NC}  Good pupy!"
 else
   echo -e "${YELLOW} [!] ${NC} Here pupy pupy!"
-  cd /root/redteam/shell/pupy
-  git clone https://github.com/n1nj4sec/pupy
+  git clone https://github.com/n1nj4sec/pupy /opt/pupy
+  cd /opt/pupy
   git submodule init
   git submodule update
   pip install -r pupy/requirements.txt
@@ -220,72 +224,54 @@ else
   rm -r payload_templates
 fi
 
-if [ -d /root/redteam/powershell/p0wnedShell ]; then
+if [ -d /opt/p0wnedShell ]; then
   echo -e "${GREEN} [*] ${NC} I feel the p0wnedShell!"  
 else
   echo -e "${YELLOW} [!] ${NC} I need p0wnedShell!"
-  cd /root/redteam/powershell
-  git clone https://github.com/Cn33liz/p0wnedShell
+  git clone https://github.com/Cn33liz/p0wnedShell /opt/p0werShell
 fi
 
-if [ -d /root/redteam/powershell/nishang ]; then
+if [ -d /opt/nishang ]; then
   echo -e "${GREEN} [*] ${NC} Got nishang!"  
 else
   echo -e "${YELLOW} [!] ${NC} git nishang!"
-  cd /root/redteam/powershell
-  git clone https://github.com/samratashok/nishang
+  git clone https://github.com/samratashok/nishang /opt/nishang
 fi
 
-if [ -d /root/redteam/powershell/PoshC2 ]; then
+if [ -d /opt/PoshC2 ]; then
   echo -e "${GREEN} [*] ${NC} This PoshC2!"  
 else
   echo -e "${YELLOW} [!] ${NC} Need more PoshC2!"
-  cd /root/redteam/powershell
-  git clone https://github.com/nettitude/PoshC2
-fi
-
-if [ -d /root/redteam/tunnels ]; then
-  echo -e "${GREEN} [*] ${NC} Can tunnel thru ICMP"  
-else
-  echo -e "${YELLOW} [!] ${NC} Need to build some ICMP tunnels!"
-  cd /root/redteam/tunnels
-  git clone https://github.com/inquisb/icmpsh
-  git clone https://github.com/friedrich/hans.git
-  git clone https://github.com/jamesbarlow/icmptunnel
-  git clone https://github.com/DhavalKapil/icmptunnel icmptunnel2
-  git clone https://github.com/Ne0nd0g/merlin
+  git clone https://github.com/nettitude/PoshC2 /opt/PoshC2
 fi
 
 ####> Recon Tools 
 
-if [ -d /root/redteam/recon/httpscreenshot ]; then
+if [ -d /opt/httpscreenshot ]; then
   echo -e "${GREEN} [*] ${NC} httpscreenshot!"  
 else
   echo -e "${YELLOW} [!] ${NC} Git httpscreenshot!"
-  cd /root/redteam/recon/
-  git clone https://github.com/breenmachine/httpscreenshot
-  cd /root/redteam/recon/httpscreenhot
+  git clone https://github.com/breenmachine/httpscreenshot /opt/httpscreenshot
+  cd /opt/httpscreenhot
   ./install-dependencies.sh
   chmod +x *.py
 fi
 
-if [ -d /root/redteam/recon/EyeWitness ]; then
+if [ -d /opt/EyeWitness ]; then
   echo -e "${GREEN} [*] ${NC} EyeWitness!"  
 else
   echo -e "${YELLOW} [!] ${NC} Git EyeWitness!"
-  cd /root/redteam/recon/
-  git clone https://github.com/ChrisTruncer/EyeWitness
-  cd /root/redteam/recon/EyeWitness/setup
+  git clone https://github.com/ChrisTruncer/EyeWitness /opt/EyeWitness
+  cd /opt/EyeWitness/setup
   ./setup.sh
 fi
 
-if [ -d /root/redteam/recon/censys-subdomain-finder ]; then
+if [ -d /opt/censys-subdomain-finder ]; then
   echo -e "${GREEN} [*] ${NC} Censys.io!"  
 else
   echo -e "${YELLOW} [!] ${NC} Git Censys.io!"
-  cd /root/redteam/recon/
-  git clone https://github.com/christophetd/censys-subdomain-finder
-  cd /root/redteam/recon/censys-subdomain-finder
+  git clone https://github.com/christophetd/censys-subdomain-finder /opt/censys-subdomain-finder
+  cd /opt/censys-subdomain-finder
   pip install -r requirements.txt
 fi
 
@@ -295,16 +281,19 @@ rtArray=(hak5/bashbunny-payloads.git BloodHoundAD/BloodHound.git sekirkity/Brows
 
 gitClone "${rtArray[@]}"
 
+rtArray=(inquisb/icmpsh friedrich/hans.git jamesbarlow/icmptunnel DhavalKapil/icmptunnel Ne0nd0g/merlin)
+
+gitClone "${rtArray[@]}"
+
 if [ -d /opt/smbexec ]; then
   echo -e "${GREEN} [*] ${NC} Got smbexec!"  
 else
   echo -e "${YELLOW} [!] ${NC} Grabbing smbexec!"
-  cd /root/recon
-  git clone https://github.com/brav0hax/smbexec.git
-  cd /root/recon/smbexec
+  git clone https://github.com/brav0hax/smbexec.git /opt/smbexec
+  cd /opt/smbexec
   apt-get install gcc-mingw-w64-i686 
   apt-get install libesedb-utils
-  sed -i '/\/opt\/esedbtools\/esedbexport/s/\/opt\/esedbtools/\/usr\/bin/' /root/recon/smbexec/smbexec.yml 
+  sed -i '/\/opt\/esedbtools\/esedbexport/s/\/opt\/esedbtools/\/usr\/bin/' /opt/smbexec/smbexec.yml 
 
 fi
 
