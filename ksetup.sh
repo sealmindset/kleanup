85a56229a5e01bfc8f2e99c3947203a5363bd0a7
sealmindset <sealmindset@gmail.com>
2018-05-12 06:57:07 -0500

    Adding more tools

--- ksetup.sh
+++ ksetup.sh
@@ -1,9 +1,15 @@
-# Setting up Dependencies
+#!/bin/bash
+#
+# Description: For building out VM based Kali Linux boxes. 
+#
+####> Add a splash of color
+
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+####> Housekeeping 
 # Dist Upgrade once
 if [ $(grep dist-upgrade /var/log/apt/history.log | wc -l) -lt 1 ]; then
   echo -e "${RED} [!!!] ${NC} Performing a Distribution Upgrade!"
@@ -15,6 +21,8 @@ else
     apt-get update
   fi
 fi
+
+####> Useful Functions 
 
 function installAPT() {
   arr=("$@")
@@ -32,12 +40,13 @@ function installAPT() {
 done
 }
 
+####> Not quite PTF, but its the tools I'm using 
+
 aptArray=(hexchat terminator exploitdb exploitdb-papers exploitdb-bin-sploits)
 installAPT "${aptArray[@]}"
 
 if [ $(type phantomjs | wc -l) -lt 1 ]; then
   echo -e "${YELLOW} [!] ${NC} Installing phantomjs!"
-  # Install PhantomJS & Imagemagick
   imgArray=(build-essential chrpath libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 imagemagick)
 installAPT "${imgArray[@]}"
   wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
@@ -84,39 +93,40 @@ if [ -f /usr/share/nmap/scripts/banner-plus.nse ]; then
   echo -e "${GREEN} [*] ${NC} banner-plus.nse exist! " 
 else
   echo -e "${YELLOW} [*] ${NC} Grabbing banner-plus.nse... " N
-  cd /root
+  cd /root/wappass
   git clone https://github.com/hdm/scan-tools.git
-  cp /root/scan-tools/nse/banner-plus.nse /usr/share/nmap/scripts/
+  cp /root/wappass/scan-tools/nse/banner-plus.nse /usr/share/nmap/scripts/
 fi
 
-if [ -d /root/w3af ]; then
+if [ -d /root/wappass/w3af ]; then
   echo -e "${GREEN} [*] ${NC} w3af is installed! " 
 else
   echo -e "${YELLOW} [*] ${NC} Grabbing w3af... " N
-  cd /root
+  cd /root/wappass
   git clone https://github.com/andresriancho/w3af.git
 
-w3afArray=(build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev)
-installAPT "${w3afArray[@]}"
-w3af2Array=(libxml2-dev libxslt1-dev zlib1g-dev)
-installAPT "${w3af2Array[@]}"
-pip install lxml==3.4.4 scapy-real==2.2.0-dev guess-language==0.2 cluster==1.1.1b3 msgpack-python==0.4.4 python-ntlm==1.0.1 halberd==0.2.4 darts.util.lru==0.5 vulndb==0.0.19 markdown==2.6.1 psutil==2.2.1 ds-store==1.1.2 termcolor==1.1.0 mitmproxy==0.13 ruamel.ordereddict==0.4.8 Flask==0.10.1 tldextract==1.7.2 pebble==4.3.6 acora==2.1 esmre==0.3.1
+  w3afArray=(build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev)
+  installAPT "${w3afArray[@]}"
+  
+  w3af2Array=(libxml2-dev libxslt1-dev zlib1g-dev)
+  installAPT "${w3af2Array[@]}"
+  pip install lxml==3.4.4 scapy-real==2.2.0-dev guess-language==0.2 cluster==1.1.1b3 msgpack-python==0.4.4 python-ntlm==1.0.1 halberd==0.2.4 darts.util.lru==0.5 vulndb==0.0.19 markdown==2.6.1 psutil==2.2.1 ds-store==1.1.2 termcolor==1.1.0 mitmproxy==0.13 ruamel.ordereddict==0.4.8 Flask==0.10.1 tldextract==1.7.2 pebble==4.3.6 acora==2.1 esmre==0.3.1
 
-  cd w3af/
-./w3af_console
-. /tmp/w3af_dependency_install.sh
+  cd /root/wappass/w3af/
+  ./w3af_console
+  ./tmp/w3af_dependency_install.sh
 fi
 
-if [ -d /root/ptf ]; then
-  echo -e "${GREEN} [*] ${NC} Got PTF!"  
-else
-  echo -e "${YELLOW} [!] ${NC} Let's get some PTF!"  
-  cd /root
-  git clone https://github.com/trustedsec/ptf.git
-  echo -e "${YELLOW} [!] ${NC} At the prompt enter"  
-  echo -e "${YELLOW} [!] ${NC} use modules/install_updata_all"  
-  echo -e "${YELLOW} [!] ${NC} yes"  
-fi
+# Not sure what the intersecs or the diffs are... yet!
+#if [ -d /root/ptf ]; then
+#  echo -e "${GREEN} [*] ${NC} Got PTF!"  
+#else
+#  echo -e "${YELLOW} [!] ${NC} Let's get some PTF!"  
+#  cd /root
+#  git clone https://github.com/trustedsec/ptf.git
+#  echo -e "${YELLOW} [!] ${NC} cd ptf"  
+#  echo -e "${YELLOW} [!] ${NC} use modules/install_updata_all"  
+#fi
 
 if [ -f /usr/bin/pwsh ]; then
   echo -e "${GREEN} [*] ${NC} PowerShell is installed! " 
@@ -138,19 +148,19 @@ else
   fi
 fi
 
-if [ -d /root/Empire ]; then
+if [ -d /root/redteam/powershell/Empire ]; then
   echo -e "${GREEN} [*] ${NC} Empire Strikes Back!"  
 else
   echo -e "${YELLOW} [!] ${NC} Git Empire...!"  
   pip install lxml==4.0.0
-  cd /root
+  cd /root/redteam/powershell
   git clone https://github.com/EmpireProject/Empire.git
-  cd /root/Empire/setup
+  cd /root/redteam/powershell/Empire/setup
   ./install.sh
   ./reset.sh
 fi
 
-if [ -d /root/dnscat2 ]; then
+if [ -d /root/redteam/dnscat2 ]; then
   echo -e "${GREEN} [*] ${NC} Meow!"  
 else
   echo -e "${YELLOW} [!] ${NC} Git some pussy...!"  
@@ -180,14 +190,60 @@ else
   echo -e "${YELLOW} [!] ${NC} Run: code --user-data-dir=\"~/.vscode-root\""
 fi
 
+if [ -f /root/redteam/shell/pupy/pupy/pupysh.py ]; then
+  echo -e "${GREEN} [!] ${NC}  Good pupy!"
+else
+  echo -e "${YELLOW} [!] ${NC} Here pupy pupy!"
+  cd /root/redteam/shell/pupy
+  git clone https://github.com/n1nj4sec/pupy
+  git submodule init
+  git submodule update
+  pip install -r pupy/requirements.txt
+  wget https://github.com/n1nj4sec/pupy/releases/download/latest/payload_templates.txz
+  tar xvf payload_templates.txz
+  mv payload_templates/* pupy/payload_templates/
+  rm payload_templates.txz
+  rm -r payload_templates
+fi
+
+if [ -d /root/redteam/powershell/p0wnedShell ]; then
+  echo -e "${GREEN} [*] ${NC} I feel the p0wnedShell!"  
+else
+  echo -e "${YELLOW} [!] ${NC} I need p0wnedShell!"
+  cd /root/redteam/powershell
+  git clone https://github.com/Cn33liz/p0wnedShell
+fi
+
+if [ -d /root/redteam/powershell/PoshC2 ]; then
+  echo -e "${GREEN} [*] ${NC} This PoshC2!"  
+else
+  echo -e "${YELLOW} [!] ${NC} Need more PoshC2!"
+  cd /root/redteam/powershell
+  git clone https://github.com/nettitude/PoshC2
+fi
+
+if [ -d /root/redteam/tunnels ]; then
+  echo -e "${GREEN} [*] ${NC} Can tunnel thru ICMP"  
+else
+  echo -e "${YELLOW} [!] ${NC} Need to build some ICMP tunnels!"
+  cd /root/redteam/tunnels
+  git clone https://github.com/inquisb/icmpsh
+  git clone https://github.com/friedrich/hans.git
+  git clone https://github.com/jamesbarlow/icmptunnel
+  git clone https://github.com/DhavalKapil/icmptunnel icmptunnel2
+fi
+
 if [ -d /root/oscp ]; then
   echo -e "${GREEN} [*] ${NC} Updating OSCP!"  
+  cd /root/oscp
   git pull
 else
   echo -e "${YELLOW} [!] ${NC} Grabbing the OSCP toolbox!"
   cd /root
   git clone https://github.com/sealmindset/oscp
 fi
+
+####> Kali Preferences
 
 if grep -q '^#.* AutomaticLogin' /etc/gdm3/daemon.conf; then
   echo -e "${YELLOW} [!] ${NC} Setting up Autologin..."
@@ -202,5 +258,7 @@ if grep -q '^disable_lua\ \= false' /etc/wireshark/init.lua; then
 else
   echo -e "${GREEN} [*] ${NC} Wireshark lua is already set."
 fi
+
+####> Done 
 
 echo -e "${GREEN} [*] ${NC} Now go hack something... (legally)"
