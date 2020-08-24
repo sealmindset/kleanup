08d8cf2ddf43ab57e0fe9f234fdfebb525618845
sealmindset <sealmindset@gmail.com>
2020-08-24 07:11:13 -0500

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tcpdump -i $(ifconfig | grep tap | cut -d":" -f1) not arp and not rarp and not 'tcp[13] & 4!=0'
+tcpdump -i $(ifconfig | grep tun | cut -d":" -f1) not arp and not rarp and not 'tcp[13] & 4!=0'
 
 #p0f -i tap0 -p -o /data/lab/results/p0f-$(date +%F_%R).log
